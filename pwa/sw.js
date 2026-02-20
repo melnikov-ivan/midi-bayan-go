ffd03afad097ab6d29c508ada634cb27a04b4259
imelnikov <melnikov.ivan@gmail.com>
2026-02-21 00:34:05 +0100

    pwa

---
+++ pwa/sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'midi-bayan-pwa-v1';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) =>
+      cache.addAll(['./index.html', './manifest.json', './icon.svg'])
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.mode !== 'navigate') return;
+  e.respondWith(
+    fetch(e.request).catch(() =>
+      caches.match(e.request).then((r) => r || caches.match('./index.html'))
+    )
+  );
+});
